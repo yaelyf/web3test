b39fafca84035cedabba373111c6976a167e1aff
Yael Yaffe <yaelyf@post.jce.ac.il>
2023-01-19 17:41:42 +0200

    lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { loadContract } from './utils/load-contract';
+import Web3 from "web3";
 
 function App() {
+
+  const [web3Api, setWeb3Api] = useState({
+    provider:null,
+    web3:null,
+    contract:null
+  })
+
+  const [balance, setBalance] = useState(null)
+  const [account, setAccount] = useState(null)
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider()
+      const contract = await loadContract("Simplebank", provider)
+
+      if(provider){
+        setWeb3Api(
+          {
+            provider:provider,
+            web3: new Web3(provider),
+            contract:contract
+          }
+        )
+      } else{
+        console.log("Please install metamask")
+      }
+    }
+  loadProvider()
+  },[])
+
+  useEffect(
+    () => {
+      const loadBalance = async () => {
+        const {contract, web3} = web3Api
+        const balance = await web3.eth.getBalance(contract.address)
+        setBalance(web3.utils.fromWei(balance, "ether"))
+      }
+      loadBalance()
+    },[web3Api])
+
+  useEffect(
+    () => {
+      const getAccount = async () =>{
+        const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+      }
+      getAccount()
+    },[web3Api.web3])
+
+    const addFunds = async () =>{
+      const {contract, web3} = web3Api
+      await contract.addFunds({
+        from:account,
+        value:web3.utils.toWei("1","ether")
+      })
+    }
+
+    const withdraw = async () => {
+      const {contract, web3} = web3Api
+      const withDrawAmount = web3.utils.toWei("0.1", "ether")
+      await contract.withdraw(withDrawAmount, {from:account})
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Current balance is {balance} Ether</div>
+      <div>Check that your account is {account}</div>
+      <div>
+        <button onClick={addFunds} >Add 1 ether</button>
+        <button onClick={withdraw} >Withdraw 1 ether</button>
+      </div>
     </div>
   );
 }
